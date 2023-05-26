e73262f76ea6843aefad3d06349ce44a080de324
jessica <jessica_02_06@outlook.com>
2023-05-25 22:27:41 -0300

    feat: instalação CORS

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "express-async-errors"
 import { handleErros } from "./errors"
 import express, { Application } from "express"
+import cors from "cors"
 import { userRoutes } from "./routers/users.routers"
 import loginRoutes from "./routers/login.routers"
 import contactRoutes from "./routers/contacts.routers"
@@ -8,6 +9,8 @@ import contactRoutes from "./routers/contacts.routers"
 
 const app: Application = express()
 app.use(express.json())
+
+app.use(cors())
 
 app.use("/users", userRoutes)
 app.use("/login", loginRoutes)
